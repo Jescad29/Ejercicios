34c3811697bf3a73a2180be8b5381ef064ae0a2b
Jesus Cadena <jesuscadena27@hotmail.com>
2024-01-26 22:18:23 -0600

    dia 10

--- Ejercicios/hangman/main.py
+++ Ejercicios/hangman/main.py
@@ -2,44 +2,54 @@ from hangman_art import logo, stages
 from hangman_words import word_list
 import random
 print(logo)
-chosen_word = random.choice(word_list)
-print(chosen_word)
 
-display = []
-lives = 6
 
-for space in range(len(chosen_word)):
-    display += "_"
+def hangman():
+    chosen_word = random.choice(word_list)
+    print(chosen_word)
+    print("Welcome to the hangman game ")
 
-end_of_game = False
+    display = []
+    lives = 6
 
-while not end_of_game:
-    guess = input("Adivina la letra de la palabra: ").lower()
+    for space in range(len(chosen_word)):
+        display += "_"
 
-    if guess in display:
-        print(stages[lives])
-        print(f"Haz adivinado!!! {guess} ")
+    end_of_game = False
 
-    for position in range(len(chosen_word)):
-        letter = chosen_word[position]
+    while not end_of_game:
+        guess = input("Adivina la letra de la palabra: ").lower()
 
-        if letter == guess:
-            # print(f"La letra {guess} si se encuentra en la palabra")
-            display[position] = letter
+        if guess in display:
+            print(stages[lives])
+            print(f"Haz adivinado!!! {guess} ")
 
-    print(display)
-    if guess not in chosen_word:
-        print("No has adivinado la letra")
-        lives -= 1
-        if lives == 0:
+        for position in range(len(chosen_word)):
+            letter = chosen_word[position]
+
+            if letter == guess:
+                # print(f"La letra {guess} si se encuentra en la palabra")
+                display[position] = letter
+
+        print(display)
+        if guess not in chosen_word:
+            print("No has adivinado la letra")
+            lives -= 1
+            if lives == 0:
+                end_of_game = True
+                print("you lose.")
+                hangman()
+
+            elif lives != 0:
+                print(f"Te quedan { lives} vidas")
+        print(f"{' '.join(display)}")
+
+        if "_" not in display:
             end_of_game = True
-            print("you lose.")
-        elif lives != 0:
-            print(f"Te quedan { lives} vidas")
-    print(f"{' '.join(display)}")
+            print("Usted gana")
+            hangman()
 
-    if "_" not in display:
-        end_of_game = True
-        print("Usted gana")
+        print(stages[lives])
 
-    print(stages[lives])
+
+hangman()
